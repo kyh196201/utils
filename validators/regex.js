1e46c35a3c3bcfc07e67db7522cefffc07cf0802
kyh196201 <kyh196201@gmail.com>
2022-01-24 09:42:35 +0900

    유틸 함수 추가 및 파일 이름 변경

--- validators/regex.js
+++ validators/regex.js
@@ -2,20 +2,32 @@
  * https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
  * 이메일 validation 체크
  */
-const validateEmail = email => {
+function validateEmail(email = '') {
 	const re =
 		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(String(email).toLowerCase());
 };
 
 // 6 ~ 20자 사이, 영어, 숫자 특수문자(_) 입력 가능한 아이디 검증
-const validateId = id => {
+function validateId(id = '') {
 	const regex = /^[\w_]{6,20}$/g; // /^([a-zA-Z0-9_]){6,20}$/g;
 
 	return regex.test(id);
 }
 
 // 빈 문자열이 아닌지 체크
-const validateNotEmpty = string => {
+function validateNotEmpty(string = '') {
 	return /\S/.test(string);
 };
+
+// 숫자로만 이루어진 문자열인지 체크
+function isOnlyNumberString(string = '') {
+	const regex = /^\d+$/;
+	return regex.test(string);
+};
+
+// 숫자를 제외한 모든 문자 제거
+// https://bobbyhadz.com/blog/javascript-remove-all-characters-except-numbres
+function removeExceptNumbers(string = '') {
+	return string.replace(/[^\d]/g, '');
+}
